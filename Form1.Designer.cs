bf2a245de0eb94c09ac5b0a810216529aadfe710
Adriaan <avandehoef@outlook.com>
2017-06-23 11:59:09 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Letterfrequenties
+{
+    partial class Letterfrequentie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Letterfrequentie));
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxWelcome = new System.Windows.Forms.TextBox();
+            this.textBoxError = new System.Windows.Forms.TextBox();
+            this.textBoxLetter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(55, 233);
+            this.textBoxInput.MaxLength = 132767;
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(739, 363);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOutput.Location = new System.Drawing.Point(55, 635);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(523, 34);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(55, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(739, 27);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Geef het karakter op om de frequentie in onderstaande tekst te bepalen";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxWelcome
+            // 
+            this.textBoxWelcome.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBoxWelcome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxWelcome.Enabled = false;
+            this.textBoxWelcome.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxWelcome.Location = new System.Drawing.Point(12, 12);
+            this.textBoxWelcome.Name = "textBoxWelcome";
+            this.textBoxWelcome.ReadOnly = true;
+            this.textBoxWelcome.Size = new System.Drawing.Size(829, 27);
+            this.textBoxWelcome.TabIndex = 3;
+            this.textBoxWelcome.Text = "Bepaal frequentie van letters, cijfers etc. in een tekst";
+            this.textBoxWelcome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxError
+            // 
+            this.textBoxError.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBoxError.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxError.Enabled = false;
+            this.textBoxError.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxError.ForeColor = System.Drawing.Color.Red;
+            this.textBoxError.Location = new System.Drawing.Point(55, 56);
+            this.textBoxError.Multiline = true;
+            this.textBoxError.Name = "textBoxError";
+            this.textBoxError.ReadOnly = true;
+            this.textBoxError.Size = new System.Drawing.Size(700, 58);
+            this.textBoxError.TabIndex = 111;
+            this.textBoxError.TabStop = false;
+            // 
+            // textBoxLetter
+            // 
+            this.textBoxLetter.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLetter.Location = new System.Drawing.Point(55, 191);
+            this.textBoxLetter.MaxLength = 1;
+            this.textBoxLetter.Name = "textBoxLetter";
+            this.textBoxLetter.Size = new System.Drawing.Size(88, 34);
+            this.textBoxLetter.TabIndex = 112;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(149, 188);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(102, 39);
+            this.btnCalculate.TabIndex = 113;
+            this.btnCalculate.Text = "bereken";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnReset.Location = new System.Drawing.Point(584, 630);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(102, 39);
+            this.btnReset.TabIndex = 114;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox1.Location = new System.Drawing.Point(55, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(88, 27);
+            this.textBox1.TabIndex = 115;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "↓↓↓↓";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox2.Location = new System.Drawing.Point(55, 602);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(88, 27);
+            this.textBox2.TabIndex = 116;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "↓↓↓↓";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.DimGray;
+            this.btnClose.Location = new System.Drawing.Point(692, 630);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(102, 39);
+            this.btnClose.TabIndex = 117;
+            this.btnClose.Text = "Afsluiten";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Letterfrequentie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(853, 707);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textBoxLetter);
+            this.Controls.Add(this.textBoxError);
+            this.Controls.Add(this.textBoxWelcome);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Letterfrequentie";
+            this.Opacity = 0.98D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Letterfrequentie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxWelcome;
+        private System.Windows.Forms.TextBox textBoxError;
+        private System.Windows.Forms.TextBox textBoxLetter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//Opdracht 4: Letterfrequenties
+//Maak een programma dat telt hoe vaak bepaalde letters in een document voorkomen.
+//Test het uit op teksten in verschillende talen.
+//Goed algoritmiseren en bedenken hoe de zaak aangepakt moet worden is hier het belangrijkst.
+
+namespace Letterfrequenties
+{
+    public partial class Letterfrequentie : Form
+    {
+        public Letterfrequentie()
+        {
+            InitializeComponent();
+        }
+
+        string letter; // de betreffende letter
+        int counter; // teller om de frequentie bij te houden
+        string input; // de tekst waarin de letters worden geteld
+        int positie; // positie in de tekst om te bepalen of het de betreffende letter is
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = "";
+            textBoxOutput.Text = "";
+            textBoxError.Text = "";
+            textBoxLetter.Text = "";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            textBoxError.Text = "";
+            input = textBoxInput.Text;
+            letter = textBoxLetter.Text;
+            counter = 0;
+
+            StringBuilder strBuilder = new StringBuilder(input);
+
+            try
+            {
+                if (textBoxLetter.Text == " " || textBoxLetter.Text == "" || textBoxLetter.Text == null)
+                {
+                    textBoxError.Text = "foutieve invoer: geef een letter, cijfer of ander (lees)teken op waarvan de frequentie kan worden bepaald";
+                }
+                else if (textBoxInput.Text == " " || textBoxInput.Text == "" || textBoxInput.Text == null)
+                {
+                    textBoxError.Text = "foutieve invoer: geen tekst gevonden om de frequentie van een bepaald karakter te bepalen";
+                }
+                else
+                {
+                    for (positie = 0; positie < input.Length; positie = positie + 1)
+                    {
+                        var character = input.ElementAt(positie);
+                        string betreffendeLetter = character.ToString();
+                        if (betreffendeLetter == letter)
+                        {
+                            counter = counter + 1;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    textBoxOutput.Text = "Het karakter " + letter + " komt " + counter + " keer voor in deze tekst";
+                }
+            }
+            catch
+            {
+                textBoxError.Text = "Er is iets fout gegaan met het verwerken van de input, probeer opnieuw";
+            }
+        }        
+    }
+}
